0ddd2545351fc4912deb150fe9c00f50c97244e4
lilei <lilei@lileismacbook-pro.local>
2016-07-06 18:39:53 +0800

    初始化pod

---
+++ baisibudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baisibudejie
+//
+//  Created by lilei on 16/7/6.
+//  Copyright © 2016年 lilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
